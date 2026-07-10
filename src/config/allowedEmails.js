270a4bc6e9e2ea68004f4a6f4fa936721a0e9812
ryanwong312 <wongryan312@gmail.com>
2026-07-10 16:33:57 +0800

    added comment, thisis final

--- src/config/allowedEmails.js
+++ src/config/allowedEmails.js
@@ -6,4 +6,5 @@ export const ALLOWED_EMAILS = [
   'wongryan312@gmail.com',
   'wongryanhk@gmail.com',
   // Add more emails here
+// make sure to go to (https://console.cloud.google.com/auth/audience?project=base44-calendar-sync-501902 ) and add email under test users
 ];
